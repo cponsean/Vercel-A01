931ead51b418fce17e92ae7db68645eb62beec8b
cponsean <CPonsean1@nait.ca>
2025-10-20 11:10:29 -0600

    add bold

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
-          <li className="mt-2 tracking-[-.01em]">
+          <li className="mt-2 tracking-[-.01em] font-bold">
             by Chalisa Ponsean
           </li>
         </ol>
